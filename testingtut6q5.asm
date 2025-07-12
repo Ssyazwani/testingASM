0cf5caf41a794087feee8915d6183b26e53f7515
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2025-07-12 10:07:53 +0800

    editing typo in comment

--- testingtut6q5.asm
+++ testingtut6q5.asm
@@ -9,7 +9,7 @@ lea si marks ;si is source index - marks would be the source
 again:
 mov al,[si]  ; al will get the value from [si] (memory which is marks)
 cmp al,47 ; comparing the [si] with 47
-jb next   ; if true that is is below 47 ( jb is jump belum), proceed to next
+jb next   ; if true that is is below 47 ( jb is jump below), proceed to next
 cmp al,49 ; comparing the [si] with 49
 ja next ; if true that is is above 49 ( ja is jump above), proceed to next
 mov[si],50 ; If AL is between 47 and 49 (inclusive), set it to 50
